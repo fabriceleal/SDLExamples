67e410624caf0ad8aac74ebc7b923bfab80dd2f8
Fabrice Leal <fabrice.leal.ch@gmail.com>
2012-12-29 08:12:34 -0800

    working!

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,30 @@
 #include <SDL/SDL.h>
 
+#define FAIL(msg)					\
+  fprintf(stderr, msg "SDL Error: %s\n", SDL_GetError());	\
+  return -1
+
+
 int main(int argc, char** argv)
 {
+  SDL_Surface* hello = NULL;
+  SDL_Surface* screen = NULL;
   SDL_Init( SDL_INIT_EVERYTHING );
+
+  screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+  hello = SDL_LoadBMP("imgs/helloworld.bmp");
+
+  if(hello == NULL) {
+    FAIL("Result of LoadBMP is null!\n");
+  }
+  
+  SDL_BlitSurface( hello, NULL, screen, NULL );
+
+  SDL_Flip( screen );
+  SDL_Delay( 2000 );
+
+  SDL_FreeSurface( hello );
+
   SDL_Quit();
   return 0;
 }
